d95f9071f811643cce2a6d654cfef997570a1576
hp <abbasabbaspour313@gmail.com>
2022-03-09 18:07:32 +0330

    Initial commit

---
+++ main/navigator.java
@@ -0,0 +1,16 @@
+package main;
+
+
+/**
+ * this class navigate us to target classes
+ */
+public class navigator {
+
+
+    /**
+     * let`s go
+     */
+    public static void main(String args[]) {
+        System.out.println("navigator");
+    }
+}
